32f4beeaeacd7afbe8f2a92074337fe4bd5b1ce1
ainimini <m13253604579@163.com>
2019-11-27 21:20:50 +0800

    feat: add UserVaccinatioInfoManagement

---
+++ src/main/java/com/fc/test/controller/admin/UserVaccinationController.java
@@ -0,0 +1,151 @@
+package com.fc.test.controller.admin;
+
+import com.fc.test.common.base.BaseController;
+import com.fc.test.common.domain.AjaxResult;
+import com.fc.test.common.log.Log;
+import com.fc.test.model.auto.TSysUserVaccinationInfo;
+import com.fc.test.model.auto.TSysVaccineInfoExample;
+import com.fc.test.model.auto.TsysUser;
+import com.fc.test.model.custom.TableSplitResult;
+import com.fc.test.model.custom.Tablepar;
+import com.fc.test.model.custom.TitleVo;
+import com.fc.test.service.SysUserService;
+import com.fc.test.service.SysVaccineInfoService;
+import com.github.pagehelper.PageInfo;
+import com.google.gson.Gson;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ * @ClassName dell
+ * @Description TOOD
+ * @Author X
+ * @Data 2019/11/21
+ * @Version 1.0
+ **/
+@Controller
+@RequestMapping("/UserVaccinationController")
+public class UserVaccinationController extends BaseController {
+
+    @Autowired
+    private SysVaccineInfoService sysVaccineInfoService;
+
+    @Autowired
+    private SysUserService sysUserService;
+
+    private String prefix = "admin/vaccination";
+
+
+    @GetMapping("/view")
+    @RequiresPermissions("system:userVaccination:view")
+    public String view(ModelMap model) {
+        String str = "用户接种疫苗信息";
+        setTitle(model, new TitleVo("列表", str + "管理", true, "欢迎进入" + str + "页面", true, false));
+        return prefix + "/list";
+    }
+
+
+    @PostMapping("/list")
+    @RequiresPermissions("system:userVaccination:list")
+    @ResponseBody
+    public Object list(Tablepar tablepar, String searchTxt) {
+        PageInfo<TSysUserVaccinationInfo> page = userVaccinationService.list(tablepar, searchTxt);
+        TableSplitResult<TSysUserVaccinationInfo> result = new TableSplitResult<TSysUserVaccinationInfo>(page.getPageNum(), page.getTotal(), page.getList());
+        return result;
+    }
+
+    /**
+     * 新增疫苗信息
+     */
+    @GetMapping("/add")
+    public String add(ModelMap modelMap) {
+        //查询疫苗数据库疫苗名称 返回给用户接种疫苗管理页面得到疫苗名称
+        modelMap.addAttribute("vaccineList", sysVaccineInfoService.selectByExample(new TSysVaccineInfoExample()));
+        //添加角色列表
+        return prefix + "/add";
+    }
+
+    @Log(title = "疫苗新增")
+    @PostMapping("/add")
+    @RequiresPermissions("system:userVaccination:add")
+    @ResponseBody
+    public AjaxResult add(TSysUserVaccinationInfo userVaccineInfo, Model model) {
+        int b = userVaccinationService.insertSelective(userVaccineInfo);
+        if (b > 0) {
+            return success();
+        } else {
+            return error();
+        }
+    }
+
+    /**
+     * 删除疫苗信息
+     *
+     * @param ids
+     * @return
+     */
+    @PostMapping("/remove")
+    @RequiresPermissions("system:userVaccination:remove")
+    @ResponseBody
+    public AjaxResult remove(String ids) {
+        int b = userVaccinationService.deleteByPrimaryKey(ids);
+        if (b > 0) {
+            return success();
+        } else {
+            return error();
+        }
+    }
+
+    /**
+     * 修改疫苗信息
+     *
+     * @param id
+     * @param mmap
+     * @return
+     */
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") String id, ModelMap mmap) {
+        //通过id查询所有接种疫苗用户信息
+        mmap.put("TSysUserVaccinationInfo", userVaccinationService.selectByPrimaryKey(id));
+        //查询疫苗数据库疫苗名称 返回给用户接种疫苗管理页面得到疫苗名称
+        mmap.addAttribute("vaccineList", sysVaccineInfoService.selectByExample(new TSysVaccineInfoExample()));
+
+        return prefix + "/edit";
+    }
+
+    /**
+     * 修改保存疫苗信息
+     */
+    @RequiresPermissions("system:userVaccination:edit")
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(TSysUserVaccinationInfo tSysUserVaccinationInfo) {
+        return toAjax(userVaccinationService.updateUserVaccinationInfo(tSysUserVaccinationInfo));
+    }
+
+    /**
+     * 通过身份证号检查用户是否存在
+     *
+     * @param TsysUser
+     * @return
+     */
+    @PostMapping("/checkVaccinationUserIdNumber")
+    @ResponseBody
+    public Object checkVaccinationUserIdNumber(@RequestParam(value = "vaccinationUserIdNumber",required = false) String vaccinationUserIdNumber) {
+        if (vaccinationUserIdNumber != ""){
+            PageInfo<TsysUser> result= userVaccinationService.checkVaccinationUserIdNumber(vaccinationUserIdNumber);
+            if(result.getSize() > 0){
+                return result;
+            }else {
+                return null;
+            }
+        }else {
+            return null;
+        }
+    }
+}
---
+++ src/main/java/com/fc/test/service/UserVaccinationService.java
@@ -0,0 +1,153 @@
+package com.fc.test.service;
+
+import com.fc.test.common.base.BaseService;
+import com.fc.test.common.support.Convert;
+import com.fc.test.mapper.auto.TSysUserVaccinationInfoMapper;
+import com.fc.test.mapper.auto.TsysUserMapper;
+import com.fc.test.mapper.custom.TsysUserDao;
+import com.fc.test.model.auto.TSysUserVaccinationInfo;
+import com.fc.test.model.auto.TSysUserVaccinationInfoExample;
+import com.fc.test.model.auto.TsysUser;
+import com.fc.test.model.auto.TsysUserExample;
+import com.github.pagehelper.PageInfo;
+import com.fc.test.model.custom.Tablepar;
+import com.fc.test.util.SnowflakeIdWorker;
+import com.github.pagehelper.PageHelper;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @ClassName dell
+ * @Description TOOD
+ * @Author X
+ * @Data 2019/11/21
+ * @Version 1.0
+ **/
+@Service
+public class UserVaccinationService implements BaseService<TSysUserVaccinationInfo, TSysUserVaccinationInfoExample> {
+
+    //生成的疫苗dao
+    @Autowired
+    private TSysUserVaccinationInfoMapper tSysUserVaccinationInfoMapper;
+
+    @Autowired
+    private TsysUserMapper tsysUserMapper;
+
+    /**
+     * 分页查询
+     *
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    public PageInfo<TSysUserVaccinationInfo> list(Tablepar tablepar, String vaccinationUserIdNumber) {
+        //模糊查询
+        TSysUserVaccinationInfoExample testExample = new TSysUserVaccinationInfoExample();
+        testExample.setOrderByClause("id+0 DESC");
+        if (vaccinationUserIdNumber != null && !"".equals(vaccinationUserIdNumber)) {
+            testExample.createCriteria().andVaccinationUserIdNumberLike("%" + vaccinationUserIdNumber + "%");
+        }
+
+        PageHelper.startPage(tablepar.getPageNum(), tablepar.getPageSize());
+        List<TSysUserVaccinationInfo> list = tSysUserVaccinationInfoMapper.selectByExample(testExample);
+        PageInfo<TSysUserVaccinationInfo> pageInfo = new PageInfo<TSysUserVaccinationInfo>(list);
+        return pageInfo;
+    }
+
+
+    @Override
+    public int deleteByPrimaryKey(String ids) {
+        List<String> lista = Convert.toListStrArray(ids);
+        TSysUserVaccinationInfoExample example = new TSysUserVaccinationInfoExample();
+        example.createCriteria().andIdIn(lista);
+        return tSysUserVaccinationInfoMapper.deleteByExample(example);
+    }
+
+    /**
+     * 添加疫苗
+     */
+    @Override
+    public int insertSelective(TSysUserVaccinationInfo record) {
+        String accineId = SnowflakeIdWorker.getUUID();
+        record.setId(accineId);
+        return tSysUserVaccinationInfoMapper.insertSelective(record);
+    }
+
+    @Override
+    public TSysUserVaccinationInfo selectByPrimaryKey(String id) {
+
+        return tSysUserVaccinationInfoMapper.selectByPrimaryKey(id);
+    }
+
+
+    @Override
+    public int updateByPrimaryKeySelective(TSysUserVaccinationInfo record) {
+        return tSysUserVaccinationInfoMapper.updateByPrimaryKeySelective(record);
+    }
+
+    @Override
+    public int updateByExampleSelective(TSysUserVaccinationInfo record, TSysUserVaccinationInfoExample example) {
+        return tSysUserVaccinationInfoMapper.updateByExampleSelective(record, example);
+    }
+
+    @Override
+    public int updateByExample(TSysUserVaccinationInfo record, TSysUserVaccinationInfoExample example) {
+
+        return tSysUserVaccinationInfoMapper.updateByExample(record, example);
+    }
+
+    @Override
+    public List<TSysUserVaccinationInfo> selectByExample(TSysUserVaccinationInfoExample example) {
+
+        return tSysUserVaccinationInfoMapper.selectByExample(example);
+    }
+
+
+    @Override
+    public long countByExample(TSysUserVaccinationInfoExample example) {
+
+        return tSysUserVaccinationInfoMapper.countByExample(example);
+    }
+
+
+    @Override
+    public int deleteByExample(TSysUserVaccinationInfoExample example) {
+
+        return tSysUserVaccinationInfoMapper.deleteByExample(example);
+    }
+
+    /**
+     * @title: updateVaccineInfo
+     * @description: 修改疫苗信息
+     * @author: X
+     * @updateTime: 2019/11/17 21:31
+     */
+    @Transactional
+    public int updateUserVaccinationInfo(TSysUserVaccinationInfo record) {
+        //更新用户信息
+        tSysUserVaccinationInfoMapper.updateByPrimaryKeySelective(record);
+        //修改用户信息
+        return 1;
+    }
+
+    /**
+     * 通过接种疫苗用户身份证号检查用户
+     *
+     * @param tSysUserVaccinationInfo
+     * @return
+     */
+    public  PageInfo<TsysUser> checkVaccinationUserIdNumber(String vaccinationUserIdNumber) {
+        TsysUserExample testExample = new TsysUserExample();
+        testExample.setOrderByClause("id+0 DESC");
+        if (vaccinationUserIdNumber != null && !"".equals(vaccinationUserIdNumber)) {
+            testExample.createCriteria().andIdNumberLike("%" + vaccinationUserIdNumber + "%");
+        }
+        List<TsysUser> list = tsysUserMapper.selectByExample(testExample);
+        PageInfo<TsysUser> pageInfo = new PageInfo<TsysUser>(list);
+        return pageInfo;
+    }
+}
